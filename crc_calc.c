d9573aeb234a954af34ffe90489296420e41e2e6
m_miled <m_miled@live.fr>
2015-08-17 13:14:05 +0100

    fonction de calcul CRC

---
+++ crc_calc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+
+unsigned char crc_calc(unsigned char* string, int length);
+
+main()
+{
+	unsigned char crc;
+	unsigned char s[]="GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W";
+	crc=crc_calc(s,strlen(s));
+	printf("%02X\n",crc);
+}
+
+unsigned char crc_calc(unsigned char* string, int length)
+{
+	unsigned char crc = 0;
+	int i;
+
+	for (i = 0; i < length; i++)
+		crc ^= string[i];
+
+	return crc;
+}
